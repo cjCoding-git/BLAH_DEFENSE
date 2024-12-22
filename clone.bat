eb9ff4d878acc751332c93122f507e9a6ea059d5
Charles Jager <charles.g.jager@gmail.com>
2024-12-22 14:04:51 -0600

    Adding clone scripts

---
+++ clone.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+ cd ..
+git clone https://github.com/cjCoding-git/BLAH_DEFENSE
+ cd .\BLAH_DEFENSE
+pause
+exit
---
+++ clone.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+ cd ..
+git clone https://github.com/cjCoding-git/BLAH_DEFENSE
+ cd ./BLAH_DEFENSE
